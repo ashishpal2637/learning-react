0b0cc263ac19f51566b65285dbb40fd25c25b49b
ashishpal2637 <ashishpal2637@gmail.com>
2025-09-09 10:55:41 +0530

    tailwind inject in react with vite

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/card'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-2xl'>Tailwind test</h1>
+      <Card />
+    </>
+  )
+}
+
+export default App
