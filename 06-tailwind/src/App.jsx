8a41c06a88bc5061dc52812f9cabfcada8999b6f
Ashish <ashishpal2637@gmail.com>
2026-03-30 15:31:36 +0530

    how tailwind css add in react

--- 06-tailwind/src/App.jsx
+++ 06-tailwind/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-900 text-white'>
+    <div className='h-full w-full bg-blue-400'>
       App
     </div>
   )
